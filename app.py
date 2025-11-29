5eee3ef4b9128512dc4fb6182f5a8a22311cf251
nixacfilms <nikhil.nixaclabs@gmail.com>
2025-11-28 23:05:26 -0500

    Trying trigger using webhook

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ DEPLOYMENT_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 @app.route('/')
 def hello():
-    return f"Hello from Richard and Nikhil. This is the version deployed on {DEPLOYMENT_TIME}"
+    return f"""Hello from Richard and Nikhil.  This is the version deployed on {DEPLOYMENT_TIME}
+
+    Triggering pipeline for CICD Assignment 4.
+"""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9876)
